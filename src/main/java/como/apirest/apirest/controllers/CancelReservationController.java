535fc1fdedf58185e3aea9abefa58955d3a75c6d
cristian gil <cristianingeniero2018@hotmail.com>
2021-06-01 14:19:54 -0500

    cancelar reservacion servicio, agregar a los controladores la anotación cross origin para poder ser consumido desde angular

--- src/main/java/como/apirest/apirest/controllers/CancelReservationController.java
+++ src/main/java/como/apirest/apirest/controllers/CancelReservationController.java
@@ -3,30 +3,28 @@ package como.apirest.apirest.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import como.apirest.apirest.exceptions.BookingException;
-import como.apirest.apirest.jsons.CreateReservationRest;
 import como.apirest.apirest.responses.BookingResponse;
-import como.apirest.apirest.servicies.ReservationService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import como.apirest.apirest.servicies.CancelReservationService;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
-public class ReservationController {
+public class CancelReservationController {
 
 	@Autowired
-	ReservationService reservationService;
+	CancelReservationService cancelReservationService;
 	
 	@ResponseStatus(HttpStatus.OK)
-	@RequestMapping(value = "reservation", method = RequestMethod.POST,  produces = MediaType.APPLICATION_JSON_VALUE)
-	public BookingResponse<String> createReservation(@RequestBody @Validated CreateReservationRest createReservationRest)
+	@RequestMapping(value = "deleteReservation", method = RequestMethod.DELETE,  produces = MediaType.APPLICATION_JSON_VALUE)
+	public BookingResponse<String> deleteReservation(@RequestParam String locator)
 			throws BookingException {
-		return new BookingResponse<>("Sucess",String.valueOf(HttpStatus.OK), "OK", reservationService.crateReservation(createReservationRest));
+		return new BookingResponse<>("Sucess",String.valueOf(HttpStatus.OK), "OK", cancelReservationService.deleteReservation(locator));
 	}
 }
--- src/main/java/como/apirest/apirest/controllers/RestaurantController.java
+++ src/main/java/como/apirest/apirest/controllers/RestaurantController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,7 +18,7 @@ import como.apirest.apirest.responses.BookingResponse;
 import como.apirest.apirest.servicies.RestaurantService;
 
 @RestController
-
+@CrossOrigin(origins = "http://localhost:4200")
 public class RestaurantController {
 	@Autowired
 	RestaurantService restaurantService;
